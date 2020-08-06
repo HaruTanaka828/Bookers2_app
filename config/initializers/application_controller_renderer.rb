82ce41443378b0c023cc14ca3b098c6130669224
vagrant <vagrant@localhost.localdomain>
2020-08-06 12:35:14 +0000

    [fix]

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '42879f471b92fa546e76e60e6d1e99694a833b9edc75da3647e74435417ef9fb62ccf68bf54570e70939f083d812f34cd2196d3904c49f8c280df27d06795c14'
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+
+  root 'home#top'
+
+  get 'home/about'
+
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+  	resource :favorites, only: [:create, :destroy]
+  	resources :book_comments, only: [:create, :destroy]
+    end
+
+  resources :users, only: [:show, :index, :edit, :update]
+
+end
